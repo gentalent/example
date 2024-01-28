151752c24d8a26a93c309d6820d28c0e13d3bb23
daito5z <157941585+daito5z@users.noreply.github.com>
2024-01-28 12:22:28 +0530

    Add files via upload

---
+++ Program1_20220010073.c
@@ -0,0 +1,13 @@
+/* Program to print my details */
+#include <stdio.h>
+
+/* Function main begins program execution */
+int main( void )
+{
+    printf( "Name: G Venkata Dheeraj Chowdary\n" );
+    printf( "Roll No: S20220010073\n" );
+    printf( "Branch: Computer Science and Engineering\n" );
+    printf( "\n" );
+    
+    return 0;/* indicate that program ended successfully */
+}/* end function main */
