b28b5b217fe40f495b1e648cef97b4b0fe69ad3e
daito5z <parzival124567@gmail.com>
2024-02-15 11:45:49 +0530

    modified Program1_20220010073

--- Program1_20220010073.c
+++ Program1_20220010073.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 
 /* Function main begins program execution */
-int main( void )
+int main(void)
 {
-    printf( "Name: G Venkata Dheeraj Chowdary\n" );
-    printf( "Roll No: S20220010073\n" );
-    printf( "Branch: Computer Science and Engineering\n" );
-    printf( "\n" );
-    
-    return 0;/* indicate that program ended successfully */
-}/* end function main */
+    printf("Name: G Venkata Dheeraj Chowdary\n");
+    printf("Roll No: S20220010073\n");
+    printf("Branch: Computer Science and Engineering\n");
+    printf("\n");
+    printf("Gowtham Erripuku");
+
+    return 0; /* indicate that program ended successfully */
+} /* end function main */
