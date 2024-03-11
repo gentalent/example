bd4ba191eb3ba884478d22ac61a86d2bbc9bf21d
daito5z <parzival124567@gmail.com>
2024-03-11 21:33:42 +0530

    login/login.js

--- login/server.js
+++ login/server.js
@@ -1,33 +1,51 @@
 const MongoClient = require("mongodb").MongoClient;
 const express = require('express');
-
-const details = require('./login')
-// const details = require('./login');
-
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 const url = "mongodb://localhost:27017";
 const port = 3000;
+let db;
 
-// MongoClient.connect(url, (err, db) => {
-//     if (err) throw err;
-//     const dbo = db.db('start');
-//     const obj = { username: 'Dheeraj', password: '1234' };
-//     dbo.collection('emp').insertOne(obj, (err) => {
-//         if (err) throw err;
-//         console.log("1 obj added");
-//     })
-// })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+})
 
-const client = new MongoClient(url);
+MongoClient.connect(url)
+    .then((client) => {
+        console.log('Connected to MongoDB');
+        db = client.db('start');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB: ', err);
+    });
 
-const database = client.db('start');
-const emp = database.collection('emp');
-emp.insertOne(details, (req, res) => {
-    if (err) throw err;
-    console.log('1 user inserted');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname)));
+
+app.post('/insertData', (req, res) => {
+    db.collection('emp').insertOne(req.body)
+        .then(() => {
+            res.sendFile(path.join(__dirname, '../home', 'home.html'));
+        })
+        .catch((err) => {
+            console.error('Error inserting data:', err);
+            res.status(500).send('Error inserting data');
+        })
+})
+
+app.get('/checkData', (req, res) => {
+    const data = req.body;
+    const { username, password } = data;
+    const details = db.collection('emp').findOne({ username: username, password: password })
+    if (details) {
+        res.sendFile(path.join(__dirname, '../home', 'home.html'));
+    }
+    else {
+        res.send('Invalid username or password');
+    }
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
